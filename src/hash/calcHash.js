125be419545a07899a3495f65c6af0e0878c21b9
Konstantin Arabei <arabey.k@gmail.com>
2023-06-19 23:50:40 +0500

    add calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import * as path from "node:path";
+import { createReadStream } from "node:fs";
+import { stdout } from "node:process";
+const { createHash } = await import("node:crypto");
+import { getDirname } from "../helpers.js";
+
+const hash = createHash("sha256");
+
+const __dirname = getDirname(import.meta.url);
+const filePath = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+  const input = createReadStream(filePath);
+  input.pipe(hash).setEncoding("hex").pipe(stdout);
 };
 
 await calculateHash();
