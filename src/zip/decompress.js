abea1eb36d60402231321549bc11f01189bb1f73
Konstantin Arabei <arabey.k@gmail.com>
2023-06-20 01:06:20 +0500

    add decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+import * as path from "node:path";
+import { pipeline } from "node:stream/promises";
+import { createReadStream, createWriteStream } from "node:fs";
+import { createGunzip } from "node:zlib";
+
+import { getDirname } from "../helpers.js";
+
+const __dirname = getDirname(import.meta.url);
+const sourceFilePath = path.join(__dirname, "files", "archive.gz");
+const destFilePath = path.join(__dirname, "files", "fileToCompress.txt");
+
 const decompress = async () => {
-    // Write your code here 
+  await pipeline(
+    createReadStream(sourceFilePath),
+    createGunzip(),
+    createWriteStream(destFilePath)
+  );
 };
 
 await decompress();
