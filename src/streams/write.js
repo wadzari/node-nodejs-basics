220e40b5060c3efd02a9a4f1c5b2dfa84aaaf2d2
Konstantin Arabei <arabey.k@gmail.com>
2023-06-20 00:15:14 +0500

    add write.js for streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import * as path from "node:path";
+import { pipeline } from "node:stream/promises";
+import { createWriteStream } from "node:fs";
+import { stdin } from "node:process";
+import { getDirname } from "../helpers.js";
+
+const __dirname = getDirname(import.meta.url);
+const filePath = path.join(__dirname, "files", "fileToWrite.txt");
+
 const write = async () => {
-    // Write your code here 
+  await pipeline(stdin, createWriteStream(filePath));
 };
 
 await write();
