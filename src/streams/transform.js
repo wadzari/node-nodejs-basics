f6c7698aed3e6e3a51f216b8d0db376824d58b7f
Konstantin Arabei <arabey.k@gmail.com>
2023-06-20 00:47:16 +0500

    add transform.js for streams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import { pipeline } from "node:stream/promises";
+import { Transform } from "node:stream";
+import { stdin, stdout } from "node:process";
+
+const reverseString = new Transform({
+  transform(chunk, encoding, callback) {
+    this.push(chunk.toString().trim().split("").reverse().join("") + "\n");
+    callback();
+  },
+});
+
 const transform = async () => {
-    // Write your code here 
+  await pipeline(stdin, reverseString, stdout);
 };
 
 await transform();
